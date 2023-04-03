97f88d92b1c78da91757bebdcb93205ab5312008
Ivan Andreev <fc97@mail.ru>
2023-04-03 19:34:27 +0400

    Save2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Ivan", "Ivanov", 35));
+        persons.add(new Person("Petr", "Ivanov-Petrov", 12));
+        persons.add(new Person("Alex", "Ivanov-Petrov-Sidorov", 42));
+        persons.add(new Person("Genri", "Petrov-Sidorov", 10));
+        persons.add(new Person("Mark", "Petrov", 27));
+
+        Collections.sort(persons, new PersonComparator());
+        System.out.println(persons);
+
     }
 }
